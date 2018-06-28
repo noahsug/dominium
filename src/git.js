bbb826ff364f311727b3c094b4424f590ac60be6
noah_sugarman <noah.sugarman@airbnb.com>
2018-06-28 12:58:07 -0700

    support multiline commit messages

--- src/git.js
+++ src/git.js
@@ -17,9 +17,9 @@ async function init() {
   Object.assign(gitInfo, await getGitInfo())
 }
 
-function getGitInfo() {
+async function getGitInfo() {
   const gitInfo = {}
-  return new Promise(resolve => {
+  await new Promise(resolve => {
     git(config.gitPath)
       .raw(['log', '--oneline', '-2'], (err, result) => {
         logError(err)
@@ -28,14 +28,26 @@ function getGitInfo() {
           change: commits[0].split(' ')[0],
           base: commits[1].split(' ')[0],
         }
-        gitInfo.commitMsg = commits[0]
-          .split(' ')
-          .slice(1)
-          .join(' ')
       })
       .revparse(['--abbrev-ref', 'HEAD'], (err, result) => {
         logError(err)
         gitInfo.branch = result.trim()
+      })
+      .exec(() => {
+        resolve()
+      })
+  })
+
+  return new Promise(resolve => {
+    git(config.gitPath)
+      .raw(['log', '--format=%B', '-1'], (err, result) => {
+        logError(err)
+        const isOneLiner = result.split('\n').length === 3
+        if (isOneLiner) {
+          gitInfo.commitMsg = result.split('\n')[0]
+        } else {
+          gitInfo.commitMsg = result
+        }
       })
       .exec(() => {
         resolve(gitInfo)
@@ -45,6 +57,7 @@ function getGitInfo() {
 
 function createPrBranch(pr, { branchSuffix, commitMsgSuffix = '' }) {
   checkGitInitCalled()
+
   return new Promise(resolve => {
     git(config.gitPath)
       .checkoutBranch(
